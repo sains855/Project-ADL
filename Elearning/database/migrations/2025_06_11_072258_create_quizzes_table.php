8b7dc60870b07ce0880ad7ac68b0d848886fcc53
MuzharMaulana <babigoreng532@gmail.com>
2025-06-11 15:23:57 +0800

    tambah table quizz

---
+++ Elearning/database/migrations/2025_06_11_072258_create_quizzes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up(): void
+{
+    Schema::create('quizzes', function (Blueprint $table) {
+        $table->id(); // id (PK)
+        $table->string('title'); // Judul
+        $table->unsignedBigInteger('subject_id'); // FK ke subjects
+        $table->dateTime('start_time'); // Mulai
+        $table->dateTime('end_time'); // Selesai
+        $table->integer('duration'); // Durasi (menit)
+        $table->timestamps();
+
+        // Foreign key
+        $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
