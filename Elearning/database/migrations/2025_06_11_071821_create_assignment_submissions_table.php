fb62cdb0d096cf5e7c9d349b2952cb4945e6e9ac
MuzharMaulana <babigoreng532@gmail.com>
2025-06-11 15:19:59 +0800

    tambah table assignment_submissions

---
+++ Elearning/database/migrations/2025_06_11_071821_create_assignment_submissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('assignment_submissions', function (Blueprint $table) {
+        $table->id(); // id (PK)
+        $table->unsignedBigInteger('assignment_id'); // FK ke assignments
+        $table->unsignedBigInteger('user_id'); // FK ke users (siswa)
+        $table->dateTime('submitted_at'); // Tanggal pengumpulan
+        $table->text('file_url'); // Link file tugas
+        $table->decimal('grade', 5, 2)->nullable(); // Nilai (opsional)
+        $table->text('feedback')->nullable(); // Komentar dari guru
+        $table->timestamps();
+
+        // Foreign keys
+        $table->foreign('assignment_id')->references('id')->on('assignments')->onDelete('cascade');
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignment_submissions');
+    }
+};
