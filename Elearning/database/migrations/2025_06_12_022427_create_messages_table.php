580deffdb088fa8e27911ae1f7464f6c3c0e38c9
MuzharMaulana <babigoreng532@gmail.com>
2025-06-12 10:25:25 +0800

    tambah table messages

---
+++ Elearning/database/migrations/2025_06_12_022427_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('messages', function (Blueprint $table) {
+        $table->id(); // id (PK)
+        $table->unsignedBigInteger('sender_id'); // FK ke users
+        $table->unsignedBigInteger('receiver_id'); // FK ke users
+        $table->text('message'); // Isi pesan
+        $table->timestamp('sent_at'); // Waktu pengiriman
+        $table->timestamps();
+
+        // Foreign keys
+        $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+        $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
