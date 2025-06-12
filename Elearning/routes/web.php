208fa234eb0cb27121f05d9f7bdd39e632373620
rasmaar <rasmaar49@gmail.com>
2025-06-12 16:53:19 +0800

    routenotif

--- Elearning/routes/web.php
+++ Elearning/routes/web.php
@@ -21,3 +21,14 @@ route::get('/dosen/dashboard', [App\Http\Controllers\DashboardController::class,
 Route::get('/mahasiswa/dashboard', function () {
     return 'Dashboard Mahasiswa';
 })->middleware('auth');
+
+use App\Http\Controllers\NotificationController;
+
+Route::get('/', [NotificationController::class, 'index']);
+
+Route::prefix('notifications')->group(function () {
+    Route::get('/tugas', [NotificationController::class, 'tugasBaru']);
+    Route::get('/pesan', [NotificationController::class, 'pesanMasuk']);
+    Route::get('/nilai', [NotificationController::class, 'nilaiTersedia']);
+    Route::get('/pengingat', [NotificationController::class, 'pengingatKelas']);
+});
